ea0a4a54706f416741620bb380586be22147f5a3
Schaich, Alonso <alonsoschaich@fastmail.fm>
2024-02-21 08:04:25 +0900

    Expose prototype for new helper function

--- regress/new-fuzzer/fuzzdataprovider.h
+++ regress/new-fuzzer/fuzzdataprovider.h
@@ -21,6 +21,7 @@ uint16_t FuzzDataReadUint16(FuzzDataProvider *data);
 uint32_t FuzzDataReadUint32(FuzzDataProvider *data);
 
 uint8_t *FuzzDataReadRemainingAsString(FuzzDataProvider *data);
+uint8_t *FuzzDataReadIntoByteArray(FuzzDataProvider *data, uint8_t *dst, size_t nmemb);
 uint8_t *FuzzDataReadByteArray(FuzzDataProvider *data, size_t nmemb);
 
 struct sockaddr FuzzDataReadSockAddr(FuzzDataProvider *data);

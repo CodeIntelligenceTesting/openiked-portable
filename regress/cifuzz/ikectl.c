f89ef1ce08ca0901b6557344f9ca8c93f80f7640
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-27 01:44:57 +0900

    Launch fuzzer

--- regress/cifuzz/ikectl.c
+++ regress/cifuzz/ikectl.c
@@ -15,16 +15,6 @@ static void setup() __attribute__ ((constructor));
 static void cleanup() __attribute__ ((destructor));
 static pthread_t g_fuzzer_thread;
 
-int LLVMFuzzerInitialize(int *argc, char ***argv)
-{
-    return 0;
-}
-
-int LLVMFuzzerTestOneInput(size_t nmemb, uint8_t *data)
-{
-    return 0;
-}
-
 void blockSignals()
 {
     sigset_t set;
@@ -44,8 +34,13 @@ void *fuzzerThreadMain(void *args)
 {
     printf("%s:%d: Fuzzer thread spawned.\n", __FILE__, __LINE__);
 
-    blockSignals();
-    printf("%s:%d: Fuzzer thread now blocking signals.\n", __FILE__, __LINE__);
+    char *args[] = {
+        "fuzzer",
+        "-runs=1",
+        NULL
+    };
+    int argsc = sizeof(args)/sizeof(args[0]);
+    LLVMFuzzerRunDriver(&argsc, &argsc, LLVMFuzzerTestOneInput);
 
     return NULL;
 }
@@ -55,11 +50,28 @@ void setup()
     printf("%s:%d: Spawning fuzzer thread...\n", __FILE__, __LINE__);
     int created = pthread_create(&g_fuzzer_thread, NULL, &fuzzerThreadMain, NULL);
     assert(created == 0);
-    int detached = pthread_detach(&g_fuzzer_thread);
+    int detached = pthread_detach(g_fuzzer_thread);
     assert(detached == 0);
 }
 
 void cleanup()
 {
     printf("%s:%d: cleanup.\n", __FILE__, __LINE__);
+}
+
+int LLVMFuzzerInitialize(int *argc, char ***argv)
+{
+    /*
+     * block signals here rather then when spawning the thread, as libFuzz might
+     * unblock them. See
+     *   https://github.com/llvm/llvm-project/blob/3ada883f7c96e099e1a665c091751bff5f16690e/compiler-rt/lib/fuzzer/FuzzerDriver.cpp#L840
+     */
+    blockSignals();
+    printf("%s:%d: Fuzzer thread now blocking signals.\n", __FILE__, __LINE__);
+    return 0;
+}
+
+int LLVMFuzzerTestOneInput(size_t nmemb, uint8_t *data)
+{
+    return 0;
 }

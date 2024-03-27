7275418a5b4cf55b4745a58a6fe7b6c3c3ee2204
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-28 01:09:08 +0900

    Make the implementation C++

---
+++ regress/cifuzz/ikectl_fuzzer.cpp
@@ -0,0 +1,41 @@
+#include <pthread.h>
+#include <unistd.h>
+
+#include <assert.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void blockSignals()
+{
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGCHLD);
+    sigaddset(&set, SIGTERM);
+    sigaddset(&set, SIGINT);
+    sigaddset(&set, SIGHUP);
+    sigaddset(&set, SIGPIPE);
+    sigaddset(&set, SIGUSR1);
+    
+    int retval = pthread_sigmask(SIG_BLOCK, &set, NULL);
+    assert(retval == 0);
+}
+
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv)
+{
+    /*
+     * block signals here rather then when spawning the thread, as libFuzz might
+     * unblock them. See
+     *   https://github.com/llvm/llvm-project/blob/3ada883f7c96e099e1a665c091751bff5f16690e/compiler-rt/lib/fuzzer/FuzzerDriver.cpp#L840
+     */
+    blockSignals();
+    printf("%s:%d: Fuzzer is now blocking signals.\n", __FILE__, __LINE__);
+    return 0;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t nmemb)
+{
+    return 0;
+}

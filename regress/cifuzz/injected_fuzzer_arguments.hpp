065ec5111b950e25b69a5760dc1873b0ff2128fd
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-29 00:04:39 +0900

    hide extern "C" from C compiler

--- regress/cifuzz/injected_fuzzer_arguments.hpp
+++ regress/cifuzz/injected_fuzzer_arguments.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
-extern "C" void injected_fuzzer_send_arguments(int argsc, char **argsv);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-extern "C" void injected_fuzzer_main_arguments(int *argsc, char ***argsv);
-extern "C" void injected_fuzzer_recv_arguments(int *argsc, char ***argsv);
+extern void injected_fuzzer_send_arguments(int argsc, char **argsv);
 
-extern "C" void injected_fuzzer_free_arguments(int *argsc, char ***argsv);
+extern void injected_fuzzer_main_arguments(int *argsc, char ***argsv);
+extern void injected_fuzzer_recv_arguments(int *argsc, char ***argsv);
+
+extern void injected_fuzzer_free_arguments(int *argsc, char ***argsv);
+
+#ifdef __cplusplus
+}
+#endif

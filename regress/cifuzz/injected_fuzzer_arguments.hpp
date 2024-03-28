e0c65e1dcf93d18211762e625557a988a69781ed
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-28 22:14:55 +0900

    fix prototypes

--- regress/cifuzz/injected_fuzzer_arguments.hpp
+++ regress/cifuzz/injected_fuzzer_arguments.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
-extern "C" void injected_fuzzer_send_arguments();
-extern "C" void injected_fuzzer_recv_arguments(int *argsc, char **argsv);
+extern "C" void injected_fuzzer_send_arguments(int argsc, char **argsv);
+extern "C" void injected_fuzzer_recv_arguments(int *argsc, char ***argsv);

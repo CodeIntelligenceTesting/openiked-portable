7ba67edc7fdfa3a6979817c927de6e5d45779bdf
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-04-04 21:38:59 +0900

    Try extracting to /

--- regress/cifuzz/ca_dispatch_ikev2.c
+++ regress/cifuzz/ca_dispatch_ikev2.c
@@ -12,7 +12,7 @@ ca_reload(struct iked *env);
 int LLVMFuzzerInitialize(int *argc, char ***argv)
 {
     printf("%s:%d: Restoring bundled configuration...\n", __FILE__, __LINE__);
-    cifuzz_bundled_config_extract(".");
+    cifuzz_bundled_config_extract("/");
     return 0;
 }
 
